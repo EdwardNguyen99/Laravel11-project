c13ef258388b5a5e28b08418f1019787f9d9d93b
EdwardNguyen99 <edwardnguyen1903@gmail.com>
2026-02-08 22:30:19 +0700

    Update route controller for User

--- modules/User/routes/routes.php
+++ modules/User/routes/routes.php
@@ -1,2 +1,10 @@
 <?php 
- use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
+use Modules\User\src\Http\Controllers\UserController;
+
+// Module User
+Route::group(['namespace' => 'Modules\User\src\Http\Controllers'], function () {
+    Route::prefix('users')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('user.index');
+    });
+});
